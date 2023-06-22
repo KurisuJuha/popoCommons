3fc752daa72209f08abcdf168bbcea016bae13d6
KYapp <kukiyomenaisapp@gmail.com>
2023-06-22 21:04:34 +0900

    Create ExtentionIEnumerableForEach.cs

---
+++ popoCommons/ExtentionIEnumerableForEach.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable PossibleMultipleEnumeration
+
+namespace JuhaKurisu.PopoTools.Commons;
+
+public static class ExtentionIEnumerableForEach
+{
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> self, Action<T> value)
+    {
+        foreach (var element in self)
+        {
+            value.Invoke(element);
+            yield return element;
+        }
+    }
+}

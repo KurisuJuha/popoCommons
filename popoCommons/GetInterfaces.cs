a67962bb42a116b78bfc8ef83f018e8631a30a2d
KYapp <kukiyomenaisapp@gmail.com>
2023-06-22 16:49:54 +0900

    Create GetInterfaces.cs

---
+++ popoCommons/GetInterfaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace JuhaKurisu.PopoTools.Commons;
+
+public static partial class PopoReflectionCommons
+{
+    public static Type[] GetInterfaces<T>()
+    {
+        return Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(
+                c => c
+                    .GetInterfaces()
+                    .Any(t => t == typeof(T))
+                )
+            .ToArray();
+    }
+}

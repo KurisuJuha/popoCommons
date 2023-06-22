c726d3eb4f0213b043f1470358f50198b90a9ac6
KYapp <kukiyomenaisapp@gmail.com>
2023-06-22 16:59:16 +0900

    NotNull

---
+++ popoCommons/ExtentionIEnumerableNotNull.cs
@@ -0,0 +1,9 @@
+﻿namespace JuhaKurisu.PopoTools.Commons;
+
+public static class ExtentionIEnumerableNotNull
+{
+    public static IEnumerable<T> NotNull<T>(this IEnumerable<T?> self)
+    {
+        return (IEnumerable<T>)self.Where(c => c != null);
+    }
+}

b7670487d9fa6424a913ab133f843fd66cd294fb
KYapp <kukiyomenaisapp@gmail.com>
2023-06-26 19:24:26 +0900

    Update ExtentionIEnumerableJoin.cs

--- popoCommons/ExtentionIEnumerableJoin.cs
+++ popoCommons/ExtentionIEnumerableJoin.cs
@@ -2,5 +2,13 @@ namespace JuhaKurisu.PopoTools.Commons;
 
 public static class ExtentionIEnumerableJoin
 {
-    
+    public static string Join<T>(this IEnumerable<T> self, char separator)
+    {
+        return string.Join(separator, self);
+    }
+
+    public static string Join<T>(this IEnumerable<T> self, string separator)
+    {
+        return string.Join(separator, self);
+    }
 }

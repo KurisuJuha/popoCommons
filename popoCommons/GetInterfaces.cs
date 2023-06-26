060205f16db2f600091634c36b4783120328bd01
Juha <kukiyomenaisapp@gmail.com>
2023-06-26 13:57:01 +0900

    すべてのassemblyからインターフェースを継承しているクラスを取得するようにした

--- popoCommons/GetInterfaces.cs
+++ popoCommons/GetInterfaces.cs
@@ -4,16 +4,14 @@ namespace JuhaKurisu.PopoTools.Commons;
 
 public static class PopoReflectionCommons
 {
+    public static IEnumerable<Type> GetInterfaces<T>(Assembly assembly)
+    {
+        return assembly.GetTypes().Where(c => c.GetInterfaces().Any(t => t == typeof(T)));
+    }
+
     public static IEnumerable<Type> GetInterfaces<T>()
     {
-        return Assembly
-            .GetExecutingAssembly()
-            .GetTypes()
-            .Where(
-                c => c
-                    .GetInterfaces()
-                    .Any(t => t == typeof(T))
-            );
+        return AppDomain.CurrentDomain.GetAssemblies().SelectMany(GetInterfaces<T>);
     }
 
     public static IEnumerable<T> CreateInterfaceInstances<T>() where T : class

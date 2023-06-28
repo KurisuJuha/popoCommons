5750209d3d34916198f80b3534203747bff04596
Juha <kukiyomenaisapp@gmail.com>
2023-06-28 10:10:06 +0900

    対象のAssemblyを指定できるように

--- popoCommons/GetInterfaces.cs
+++ popoCommons/GetInterfaces.cs
@@ -4,14 +4,23 @@ namespace JuhaKurisu.PopoTools.Commons;
 
 public static class PopoReflectionCommons
 {
-    public static IEnumerable<Type> GetInterfaces<T>(Assembly assembly)
+    public static IEnumerable<Type> GetInterfaces<T>(params Assembly[] assemblies)
     {
-        return assembly.GetTypes().Where(c => c.GetInterfaces().Any(t => t == typeof(T)));
+        return assemblies.SelectMany(assembly =>
+            assembly.GetTypes().Where(c => c.GetInterfaces().Any(t => t == typeof(T))));
     }
 
     public static IEnumerable<Type> GetInterfaces<T>()
     {
         return AppDomain.CurrentDomain.GetAssemblies().SelectMany(GetInterfaces<T>);
+    }
+
+    public static IEnumerable<T> CreateInterfaceInstances<T>(params Assembly[] assemblies) where T : class
+    {
+        return GetInterfaces<T>(assemblies)
+            .Where(c => !c.IsAbstract)
+            .Select(c => Activator.CreateInstance(c) as T)
+            .NotNull();
     }
 
     public static IEnumerable<T> CreateInterfaceInstances<T>() where T : class

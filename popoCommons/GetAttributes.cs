a99e085fc4b4d730dcaa7ee031deb9884b19461b
Juha <kukiyomenaisapp@gmail.com>
2023-07-05 09:53:28 +0900

    Update GetAttributes.cs

--- popoCommons/GetAttributes.cs
+++ popoCommons/GetAttributes.cs
@@ -1,6 +1,16 @@
+using System.Reflection;
+
 namespace JuhaKurisu.PopoTools.Commons;
 
-public class GetAttributes
+public partial class PopoReflectionCommons
 {
-    
+    public PropertyInfo[] GetProperties<T>(BindingFlags bindingFlags)
+    {
+        return typeof(T).GetProperties(bindingFlags);
+    }
+
+    public MethodInfo[] GetMethods<T>(BindingFlags bindingFlags)
+    {
+        return typeof(T).GetMethods(bindingFlags);
+    }
 }
